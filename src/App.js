98edee85fd5d5f65fd66e9b94dd340c2d81b73e2
Fumihiko Hata <hatappo@users.noreply.github.com>
2021-10-21 20:32:48 +0900

    finished the getting started.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { TinaProvider, TinaCMS, useCMS, useForm, usePlugin } from 'tinacms';
 import logo from './Icon.svg';
 import './App.css';
 
 function App() {
+  const cms = new TinaCMS({
+    sidebar: true,
+  });
   return (
-    <div className="App">
-      <PageContent />
-    </div>
+    <TinaProvider cms={cms}>
+      <div className="App">
+        <PageContent />
+      </div>
+    </TinaProvider>
   );
 }
 
@@ -18,20 +24,78 @@ const pageData = {
 };
 
 function PageContent() {
+  const cms = useCMS();
+  // 2. Define the form configuration object
+  const formConfig = {
+    id: 'tina-tutorial-index',
+    label: 'Edit Page',
+    fields: [
+      {
+        name: 'title',
+        label: 'Title',
+        component: 'text',
+      },
+      {
+        name: 'body',
+        label: 'Body',
+        component: 'textarea',
+      },
+    ],
+    // initialValues: pageData,
+    async loadInitialValues() {
+      return fetch(
+        'https://jsonplaceholder.typicode.com/posts/1'
+      ).then((response) => response.json());
+    },
+    // onSubmit: async () => {
+    //   window.alert('Saved!')
+    // },
+    async onSubmit(formData) {
+      cms.alerts.info('Saving Content...')
+      return fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'PUT',
+        body: JSON.stringify({
+          id: 1,
+          title: formData.title,
+          body: formData.body,
+          userId: 1,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(e => console.error(e))
+    },
+
+  }
+
+  // 3. Create the form
+  const [editableData, form] = useForm(formConfig)
+
+  // 4. Register it with the CMS
+  usePlugin(form)
+
   return (
     <section className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1>{pageData.title}</h1>
-      <p>{pageData.body}</p>
+      {/**
+       * 5. Render the `editableData` returned from `useForm`
+       */}
+      <h1>{editableData.title}</h1>
+      <p>{editableData.body}</p>
       <EditButton />
     </section>
   );
 }
 
 function EditButton() {
+  const cms = useCMS();
+
   return (
-    <button onClick={() => window.alert("Tina isn't configured yet!")}>
-      Edit This Site
+    <button onClick={() => cms.toggle()}>
+      {cms.enabled ? 'Exit Edit Mode' : 'Edit This Site'}
     </button>
   );
 }
